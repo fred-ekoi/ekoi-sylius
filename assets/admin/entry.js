6ec33e1ed09303ff0751d08fd524f6d372abbb56
vincent-landeau <vincent.landeau.contact@gmail.com>
2024-08-28 12:01:22 +0200

    add js to admin/taxon page

--- assets/admin/entry.js
+++ assets/admin/entry.js
@@ -6,12 +6,15 @@ import './styles/app.scss';
 import MenuItemForm from "./js/menuItemForm.js";
 import MenuPageForm from "./js/menuPageForm.js";
 import CategoryPromotion from "./js/categoryPromotion.js";
+import Taxon from './js/taxon.js';
 
 const menuItemForm = new MenuItemForm();
 const menuPageForm = new MenuPageForm();
 const categoryPromotion = new CategoryPromotion();
+const taxon = new Taxon();
 document.addEventListener("DOMContentLoaded", () => {
   menuItemForm.init();
   menuPageForm.init(); 
   categoryPromotion.init();
+  taxon.init();
 });
---
+++ assets/admin/js/taxon.js
@@ -0,0 +1,37 @@
+class Taxon {
+  imageWrapper = null;
+
+  constructor() {}
+
+  init = () => {
+    console.log("Taxon init");
+    if (document.querySelector('form[name="sylius_taxon"]')) {
+      this.imageWrapper = document.querySelector(`.upload[id="sylius_taxon"]`);
+
+      this.setEvents();
+    }
+  };
+
+  setEvents = () => {
+    this.imageWrapper.addEventListener("change", () => {
+      this.updatePreview(this.imageWrapper);
+      // console.log(this.imageWrapper);
+      
+    });
+  };
+
+  updatePreview = imageWrapper => {
+    const image = imageWrapper.querySelector("img");
+    const file = imageWrapper.querySelector("input[type=file]").files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        image.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+      image.classList.remove("hidden");
+    }
+  };
+}
+
+export default Taxon;
