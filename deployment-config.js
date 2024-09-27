f94e2ac5eedcb30ae71a5f396df2a7a45568b41f
vincent-landeau <vincent@16h33.fr>
2024-09-27 11:39:46 +0200

    deploy

---
+++ deployment-config.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function afterDeploy(context) {
+    return [`php-8.2 ${context.release.path}/bin/console d:m:m -n`, `php-8.2 ${context.release.path}/bin/console c:c`];
+}
+
+module.exports = function (options) {
+    return {
+        common: {
+            mode: "synchronize",
+            currentReleaseLink: "current",
+            localPath: "./", //Warning : Do not leave blank !
+            share: {
+                uploads: "public/uploads",
+                "media-cache": "public/media/cache",
+                "translations-dir": "translations",
+                log: "var/log",
+            },
+            exclude: [".github/**", ".github", "assets/**", "assets", "tests/**", "tests", ".git/**", ".git"],
+            create: ["var", "var/log"],
+            onAfterDeploy: afterDeploy,
+        },
+
+        environments: {
+            preprod: {
+                host: process.env.SSH_HOST,
+                username: process.env.SSH_LOGIN,
+                password: process.env.SSH_PWD,
+                port: 22,
+                deployPath: process.env.SSH_PATH,
+            },
+            prod: {
+                host: process.env.SSH_HOST,
+                username: process.env.SSH_LOGIN,
+                password: process.env.SSH_PWD,
+                port: 22,
+                deployPath: process.env.SSH_PATH,
+            },
+        },
+    };
+};
