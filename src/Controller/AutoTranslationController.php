010fcb479c5f711f1f976a11174e372ff802220a
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-10-02 12:09:10 +0200

    feat: map dto and inject open ai client for testing

--- src/Controller/AutoTranslationController.php
+++ src/Controller/AutoTranslationController.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\Translation\AutoTranslationBodyDTO;
+use OpenAI\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AutoTranslationController extends AbstractController
 {
     #[Route('/admin/auto-translation', methods: ['POST'])]
-    public function index(Request $request): Response
+    public function index(
+        #[MapRequestPayload] AutoTranslationBodyDTO $data,
+        Client $openAI
+    ): Response
     {
-        $data = $request->request->all();
-        return $this->json(['message' => 'Hello world']);
+        try {
+            $result = $openAI->completions()->create([
+                'prompt' => "Hello !",
+                'model' => 'gpt-4o-mini-2024-07-18'
+            ]);
+            return $this->json($result);
+        } catch (\OpenAI\Exceptions\ErrorException $e) {
+            return $this->json(['error' => $e->getErrorMessage()], 500);
+        }
     }
 }
