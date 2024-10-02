e4751ba31ea90e7e30389b4c5f9c1f78a996221c
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-10-02 09:38:35 +0200

    fix: change menu item and menu page to menu in js

--- assets/admin/entry.js
+++ assets/admin/entry.js
@@ -3,23 +3,20 @@
 
 // In this file you can import assets like images or stylesheets
 import './styles/app.scss';
-import MenuItemForm from "./js/menuItemForm.js";
-import MenuPageForm from "./js/menuPageForm.js";
 import CategoryPromotion from "./js/categoryPromotion.js";
 import CategoryOutfit from "./js/categoryOutfit.js";
 import Taxon from './js/taxon.js';
 import AutoTranslation from './js/autoTranslation.js';
+import Menu from "./js/menu";
 
-const menuItemForm = new MenuItemForm();
-const menuPageForm = new MenuPageForm();
+const menu = new Menu();
 const categoryPromotion = new CategoryPromotion();
 const categoryOutfit = new CategoryOutfit();
 const taxon = new Taxon();
 const autoTranslation = new AutoTranslation();
 
 document.addEventListener("DOMContentLoaded", () => {
-  menuItemForm.init();
-  menuPageForm.init();
+  menu.init();
   categoryPromotion.init();
   categoryOutfit.init();
   taxon.init();
