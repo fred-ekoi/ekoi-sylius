b1e22abc58dc973d28bab32a502f0836bdc7ee08
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-10-22 14:30:32 +0200

    feat(translations): create instructions for openai and return result

--- src/Controller/AutoTranslationController.php
+++ src/Controller/AutoTranslationController.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\DTO\Translation\AutoTranslationBodyDTO;
-use App\Entity\Translation\TranslationOverrideDictionary;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\Translation\TranslationOverrideDictionaryRepository;
 use OpenAI\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,79 +14,83 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AutoTranslationController extends AbstractController
 {
+    private TranslationOverrideDictionaryRepository $repository;
+
+    public function __construct(TranslationOverrideDictionaryRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     #[Route('/admin/auto-translation', methods: ['POST'])]
     public function index(
         #[MapRequestPayload] AutoTranslationBodyDTO $data,
-        Client $openAI,
-        EntityManagerInterface $entityManager
-    ): Response
+        Client $openAI): Response
     {
-        $systemInstructions = <<<INSTRUCTIONS
+        // Validate the request body
+        $defaultLocale = $data->getData()[0]['locale'];
+        $targetLocales = $data->getTargetLocales();
+        if (!isset($defaultLocale) || !isset($targetLocales)) {
+            return $this->json([
+                'error' => 'Invalid request body'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Fetch the relevant overrides
+        $overrides = $this->repository->findRelevant($defaultLocale, $targetLocales);
+        $requestStr = "";
+        // Prepare the request string with the overrides
+        foreach ($overrides as $override) {
+            $keyText = $override->getKeyText();
+            $keyLocale = $override->getKeyLocale()->getCode();
+            $valueText = $override->getValueText();
+            $valueLocale = $override->getValueLocale()->getCode();
+            $requestStr .= <<<EOT
+            "$keyText" ($keyLocale) => "$valueText" ($valueLocale)
+
+            EOT;
+        }
+        $encodedData = json_encode($data->getData());
+        $encodedTargetLocales = json_encode($targetLocales);
+        // Append the data and targetLocales to the request string
+        $requestStr .= <<<EOT
+
+        {"data": $encodedData,"targetLocales": $encodedTargetLocales}
+        EOT;
+
+        // Define the instructions for the OpenAI model
+        $instructions = <<<EOT
         You will receive two inputs and return a JSON output with translations in a specific format.
-        The first input is a list of key/value pairs with their corresponding locales, structured like this:
-        "key"(locale) => "value"(locale)
-        You must respect the specific requirements of this list during translation, ensuring the key is used in the correct locale, and that the value is translated according to its locale. Only adapt the translation when it aligns with the locale of the value.
+        The first input is a list of key/value pairs with their corresponding locales, structured like this: "key" (locale) => "value" (locale)
+        You must respect the specific requirements of this list during translation, ensuring the key is used in the correct locale, and that the value is translated according to its locale. Only adapt the translation when it aligns with the locale of the value. You can ignore case for this input.
         The second input will look like this:
-        {"data": [{"locale": "fr_FR","name": "name","value": "Salut"},{"locale": "fr_FR","name": "slug","value": "salut"}],"targetLocales": ["en", "es"]}
+        {"data": [{"locale": "fr_FR","name": "name","value": "Combinaison vélo"},{"locale": "fr_FR","name": "slug","value": "combinaison-velo"}],"targetLocales": ["en", "es"]}
         The data array contains objects with three properties:
         * locale: The locale of the text
         * name: The field name of the input, which needs to be taken into account. For instance, a "slug" should follow the proper format (no accents, spaces, etc.)
         * value: The text to be translated
         The targetLocales array specifies the locales into which you need to translate the data.
         Your output should be a JSON array structured as follows:
-        {"data": [{"locale": "en","localeData": [{"name": "name","value": "Hi"},{"name": "slug","value": "hi"}]},{"locale": "es","localeData": [{"name": "name","value": "Hola"},{"name": "slug","value": "hola"}]}]}
+        {"data": [{"locale": "en","localeData": [{"name": "name","value": "Bike suit"},{"name": "slug","value": "bike-suit"}]},{"locale": "es","localeData": [{"name": "name","value": "Traje de ciclismo"},{"name": "slug","value": "traje-de-ciclismo"}]}]}
         Ensure you respect the list of key/value pairs for overriding translations in specific locales. And respect absolutely the output format.
-        INSTRUCTIONS;
+        EOT;
 
-        $responseStr = <<<RESPONSE
-        {
-          "data": [
-            {
-              "locale": "en",
-              "localeData": [
-                {
-                  "name": "name",
-                  "value": "Total jumpsuit"
-                },
-                {
-                  "name": "slug",
-                  "value": "total-jumpsuit"
-                },
-                {
-                  "name": "description",
-                  "value": "It's a great jumpsuit. It keeps you warm in the winter and ventilates well in the summer. A jewel of technology!"
-                },
-                {
-                  "name": "shortDescription",
-                  "value": "Top-notch jumpsuit."
-                }
-              ]
-            },
-            {
-              "locale": "fr",
-              "localeData": [
-                {
-                  "name": "name",
-                  "value": "Combinaison totale"
-                },
-                {
-                  "name": "slug",
-                  "value": "combinaise-totale"
-                },
-                {
-                  "name": "description",
-                  "value": "C'est une super combinaison. Elle tient chaud l'hiver et ventile bien l'été. Un bijou de technologie !"
-                },
-                {
-                  "name": "shortDescription",
-                  "value": "Combinaison au top."
-                }
-              ]
-            }
-          ]
-        }
-        RESPONSE;
-        $response = json_decode($responseStr, true);
-        return $this->json($response);
+        // Call the OpenAI model
+        $openAIResponse = $openAI->chat()->create([
+            'model' => 'gpt-4o-mini',
+            'messages' => [
+                [
+                    'role' => 'system',
+                    'content' => $instructions
+                ],
+                [
+                    'role' => 'user',
+                    'content' => $requestStr
+                ]
+            ],
+        ]);
+        // TODO: Handle possible errors
+        $resultStr = $openAIResponse->choices[0]->message->content;
+        $resultJson = json_decode($resultStr, true);
+        return $this->json($resultJson);
     }
 }
--- src/Repository/Translation/TranslationOverrideDictionaryRepository.php
+++ src/Repository/Translation/TranslationOverrideDictionaryRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository\Translation;
 
 use App\Entity\Translation\TranslationOverrideDictionary;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 /**
@@ -10,6 +12,33 @@ use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
  */
 class TranslationOverrideDictionaryRepository extends EntityRepository
 {
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct($em, new ClassMetadata(TranslationOverrideDictionary::class));
+    }
+
+    /**
+     * Find all the translation overrides that are relevant to the given key and value locales
+     * @param string $keyLocale The locale of the key
+     * @param string[] $valueLocales The locales of the values
+     * @return TranslationOverrideDictionary[]
+     */
+    public function findRelevant(string $keyLocale, array $valueLocales): array {
+        return $this->createQueryBuilder('t')
+            ->leftJoin('t.keyLocale', 'keyLocale')
+            ->leftJoin('t.valueLocale', 'valueLocale')
+            ->andWhere('keyLocale.code = :keyLocale')
+            ->andWhere('valueLocale.code IN (:valueLocales)')
+            ->setParameter('keyLocale', $keyLocale)
+            ->setParameter('valueLocales', $valueLocales)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     //    /**
     //     * @return TranslationOverrideDictionary[] Returns an array of TranslationOverrideDictionary objects
     //     */
