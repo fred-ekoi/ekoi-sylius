fa817afc2f436a4a7cf987dc4655ea8e66d3fa3b
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-10-25 15:01:56 +0200

    feat(translations): render templates route

--- src/Controller/AdminAutoTranslationController.php
+++ src/Controller/AdminAutoTranslationController.php
@@ -5,27 +5,31 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\DTO\Translation\AutoTranslationBodyDTO;
+use App\Entity\Locale\Locale;
 use App\Repository\Translation\TranslationOverrideDictionaryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use OpenAI\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
-class AutoTranslationController extends AbstractController
+class AdminAutoTranslationController extends AbstractController
 {
-    private TranslationOverrideDictionaryRepository $repository;
+    private TranslationOverrideDictionaryRepository $overrideRepository;
 
-    public function __construct(TranslationOverrideDictionaryRepository $repository)
+    public function __construct(TranslationOverrideDictionaryRepository $overrideRepository)
     {
-        $this->repository = $repository;
+        $this->overrideRepository = $overrideRepository;
     }
 
     #[Route('/admin/auto-translation', methods: ['POST'])]
     public function index(
         #[MapRequestPayload] AutoTranslationBodyDTO $data,
-        Client $openAI): Response
-    {
+        Client $openAI
+    ): Response {
         // Validate the request body
         $defaultLocale = $data->getData()[0]['locale'];
         $targetLocales = $data->getTargetLocales();
@@ -36,7 +40,7 @@ class AutoTranslationController extends AbstractController
         }
 
         // Fetch the relevant overrides
-        $overrides = $this->repository->findRelevant($defaultLocale, $targetLocales);
+        $overrides = $this->overrideRepository->findRelevant($defaultLocale, $targetLocales);
         $requestStr = "";
         // Prepare the request string with the overrides
         foreach ($overrides as $override) {
@@ -60,7 +64,7 @@ class AutoTranslationController extends AbstractController
         // Define the instructions for the OpenAI model
         $instructions = <<<EOT
         You will receive two inputs and return a JSON output with translations in a specific format.
-        The first input is a list of key/value pairs with their corresponding locales, structured like this: "key" (locale) => "value" (locale)
+        The first input is a list of key/value pairs with their corresponding locales, structured like this:"key" (locale) => "value" (locale)
         You must respect the specific requirements of this list during translation, ensuring the key is used in the correct locale, and that the value is translated according to its locale. Only adapt the translation when it aligns with the locale of the value. You can ignore case for this input.
         The second input will look like this:
         {"data": [{"locale": "fr_FR","name": "name","value": "Combinaison vélo"},{"locale": "fr_FR","name": "slug","value": "combinaison-velo"}],"targetLocales": ["en", "es"]}
@@ -73,24 +77,63 @@ class AutoTranslationController extends AbstractController
         {"data": [{"locale": "en","localeData": [{"name": "name","value": "Bike suit"},{"name": "slug","value": "bike-suit"}]},{"locale": "es","localeData": [{"name": "name","value": "Traje de ciclismo"},{"name": "slug","value": "traje-de-ciclismo"}]}]}
         Ensure you respect the list of key/value pairs for overriding translations in specific locales. And respect absolutely the output format.
         EOT;
-
-        // Call the OpenAI model
-        $openAIResponse = $openAI->chat()->create([
-            'model' => 'gpt-4o-mini',
-            'messages' => [
-                [
-                    'role' => 'system',
-                    'content' => $instructions
+        try {
+            // Call the OpenAI model
+            $openAIResponse = $openAI->chat()->create([
+                'model' => 'gpt-4o-mini',
+                'messages' => [
+                    [
+                        'role' => 'system',
+                        'content' => $instructions
+                    ],
+                    [
+                        'role' => 'user',
+                        'content' => $requestStr
+                    ]
                 ],
-                [
-                    'role' => 'user',
-                    'content' => $requestStr
-                ]
-            ],
-        ]);
-        // TODO: Handle possible errors
-        $resultStr = $openAIResponse->choices[0]->message->content;
-        $resultJson = json_decode($resultStr, true);
+            ]);
+            if (!isset($openAIResponse->choices[0]->message->content)) {
+                throw new \Exception('Erreur lors de la communication avec OpenAI');
+            }
+            $resultStr = $openAIResponse->choices[0]->message->content;
+            $resultJson = json_decode($resultStr, true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new \Exception('Erreur lors du décodage de la réponse JSON');
+            }
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 500);
+        }
         return $this->json($resultJson);
+    }
+
+    #[Route('/admin/auto-translation', methods: ['GET'])]
+    public function show(Request $request, EntityManagerInterface $em, TranslatorInterface $translator): Response
+    {
+        $localesCode = $request->query->all('locales');
+
+        if (empty($localesCode)) {
+            return $this->json([
+                'error' => 'Invalid request. No locales provided.'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $localeObjects = $em->getRepository(Locale::class)->findBy(['code' => $localesCode]);
+        $locales = [];
+        foreach ($localeObjects as $locale) {
+            $locales[] = [
+                'localeName' => $locale->getName(),
+                'localeCode' => $locale->getCode()
+            ];
+        }
+
+        $button = $this->render('bundles/SyliusAdminBundle/Translation/translateButton.html.twig')->getContent();
+        $modal = $this->render('bundles/SyliusAdminBundle/Translation/modalTranslation.html.twig', [
+            'locales' => $locales
+        ])->getContent();
+        return $this->json([
+            'button' => $button,
+            'modal' => $modal
+        ]);
     }
 }
