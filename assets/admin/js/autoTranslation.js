d07c04e032e1b218389907f480c23145fd015fef
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-10-25 15:02:53 +0200

    feat(translations): add handle templates in front

--- assets/admin/js/autoTranslation.js
+++ assets/admin/js/autoTranslation.js
@@ -24,7 +24,7 @@ class AutoTranslation {
    * @type {HTMLElement | null}
    * div that contains fr_FR inputs
    */
-  defaultContainer= null
+  defaultContainer = null
 
   /**
    * Translated data
@@ -48,6 +48,18 @@ class AutoTranslation {
   modal = null;
 
   /**
+   * Loader
+   * @type {HTMLElement | null}
+   */
+  loader = null;
+
+  /**
+   * Error paragraph
+   * @type {HTMLElement | null}
+   */
+  errorDiv = null;
+
+  /**
    * Initialize the AutoTranslation class.
    */
   init() {
@@ -56,9 +68,32 @@ class AutoTranslation {
 
     if (this.accordion === null || this.defaultContainer === null) return;
 
-    this.appendTranslateButton();
-    this.setTranslateButtonEvent();
-    this.handleModal();
+    //TODO: refactor this
+    this.fetchTemplates();
+    // this.appendTranslateButton();
+  }
+
+  /**
+   * Fetch templates from custom route.
+   */
+  fetchTemplates() {
+    const locales = this.findOtherLocales();
+    const query = locales.map((locale) => `locales[]=${locale}`).join('&');
+    fetch(`/admin/auto-translation?${query}`)
+      .then((response) => response.json())
+      .then((response) => {
+        const domParser = new DOMParser();
+        const buttonDiv = domParser.parseFromString(response.button, 'text/html').body.firstElementChild;
+        console.log(buttonDiv);
+        this.translateButton = buttonDiv.querySelector('#translateButton');
+        this.errorDiv = buttonDiv.querySelector('#errorDiv');
+        this.accordion.after(buttonDiv);
+        this.loader = this.translateButton.querySelector('.ui.loader');
+        this.modal = domParser.parseFromString(response.modal, 'text/html').body.firstElementChild;
+        document.body.insertAdjacentElement('beforeend', this.modal);
+        this.setTranslateButtonEvent();
+        this.handleModal();
+      });
   }
 
   /**
@@ -66,103 +101,24 @@ class AutoTranslation {
    * Then call {@link handleConfirmButton} with languages that we select
    */
   handleModal() {
-    const modalHtml = `
-            <div id="translationModal" class="modal" style="display:none;">
-              <div class="modal-content">
-                <span class="close" id="closeTranslationModal">&times;</span>
-                <p>Sélectionnez les langues souhaitées :</p>
-                <form>
-                    <button class="ui primary button" id="submitTranslationModal" type="submit">Confirmer</button>
-                </form>
-              </div>
-            </div>
-
-            <style>
-              .modal {
-                display: none;
-                position: fixed;
-                z-index: 1000;
-                padding-top: 60px;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                overflow: auto;
-                background-color: rgba(0, 0, 0, 0.4);
-              }
-
-              .modal .modal-content {
-                background-color: #fefefe;
-                margin: 50px auto;
-                padding: 20px;
-                border: 1px solid #888;
-                max-width: 500px;
-                width: 80%;
-              }
-
-              .modal .close {
-                color: #aaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-              }
-
-              .modal .close:hover,
-              .modal .close:focus {
-                color: black;
-                text-decoration: none;
-                cursor: pointer;
-              }
-
-              .modal form {
-                display: flex;
-                flex-direction: column;
-                gap: 1rem;
-                padding-bottom: 20px;
-              }
-
-              .modal label {
-                display: flex;
-                gap: 1rem;
-                align-items: center;
-              }
-            </style>
-        `;
-
-    document.body.insertAdjacentHTML('beforeend', modalHtml);
-
     // Get the modal
-    const modal = document.getElementById('translationModal');
-    this.modal = modal;
-    modal.onclick = function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
+    console.log(this.modal, this.modal.onclick)
+    this.modal.onclick = function (event) {
+      if (event.target === this.modal) {
+        this.modal.style.display = "none";
       }
-    }
+    }.bind(this);
 
-    // Get form element
-    const form = modal.getElementsByTagName('form')[0];
-    const locales = this.findOtherLocales();
-    locales.forEach((locale) => {
-      // Create checkbox for each locale
-      const label = document.createElement('label');
-      label.innerHTML = `
-        <input type="checkbox" name="${locale}" value="${locale}" checked>
-        <span>${locale}</span>
-      `;
-      // Append checkbox to form
-      form.insertAdjacentElement('afterbegin',
-        label
-      );
-    })
 
     // Get the <span> element that closes the modal
     const closeBtn = document.getElementById('closeTranslationModal');
-    closeBtn.onclick = function() {
-      modal.style.display = "none";
-    }
+    closeBtn.onclick = function () {
+      this.modal.style.display = "none";
+    }.bind(this)
 
-    form.onsubmit = function(event) {
+    // Get form element
+    const form = this.modal.getElementsByTagName('form')[0];
+    form.onsubmit = function (event) {
       event.preventDefault();
       // Get all checkboxes that we create
       const checkboxes = form.querySelectorAll('input[type="checkbox"]');
@@ -178,24 +134,9 @@ class AutoTranslation {
         return;
       }
       this.handleConfirmButton(locales);
-      modal.style.display = "none";
+      this.modal.style.display = "none";
+      this.loader.style.display = 'inline-block';
     }.bind(this)
-  }
-
-  /**
-   * Create translate button with primary button style and margin top.
-   * And then append it after accordion.
-   */
-  appendTranslateButton() {
-    this.translateButton = document.createElement('button');
-    this.translateButton.className = 'ui labeled icon primary button';
-    this.translateButton.innerHTML = `
-      <i class="translate icon"></i>
-      Traduire
-    `;
-    this.translateButton.style.marginTop = '1rem';
-
-    this.accordion.after(this.translateButton);
   }
 
   /**
@@ -203,7 +144,6 @@ class AutoTranslation {
    */
   setTranslateButtonEvent() {
     if (this.translateButton === null) return;
-
     this.translateButton.addEventListener('click', (event) => {
       event.preventDefault();
       this.modal.style.display = "block";
@@ -233,6 +173,7 @@ class AutoTranslation {
         });
       }
     });
+    this.errorDiv?.classList.add('hidden');
     this.fetchTranslations({targetLocales, data});
   }
 
@@ -252,12 +193,29 @@ class AutoTranslation {
     })
       .then((response) => response.json())
       .then((response) => {
+        this.loader.style.display = 'none';
         if ('data' in response) {
           this.translatedData = response;
           this.fillTranslatedInputs();
           this.fillNonTranslatedInputs();
+        } else if ('error' in response) {
+          this.setError(response.error);
         }
+      })
+      .catch((error) => {
+        this.setError('Une erreur s\'est produite lors de la traduction.');
       });
+  }
+
+  /**
+   * Set error message
+   *
+   * @param {string} message - Error message
+   */
+  setError(message) {
+    const errorParagraph = this.errorDiv.querySelector('p');
+    errorParagraph.innerText = message;
+    this.errorDiv?.classList.remove('hidden');
   }
 
   /**
@@ -282,9 +240,9 @@ class AutoTranslation {
   fillTranslatedInputs() {
     if (this.translatedData === null) return;
 
-    const { data } = this.translatedData;
+    const {data} = this.translatedData;
     data.forEach((translatedData) => {
-      const { locale, localeData } = translatedData;
+      const {locale, localeData} = translatedData;
       const container = document.querySelector(`.accordion div[data-locale="${locale}"] .content`);
       if (container === null) return;
 
